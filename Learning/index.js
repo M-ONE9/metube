b2b06f42a0323282a62e0181852c58e8310e61a1
M-ONE9 <116742729+M-ONE9@users.noreply.github.com>
2022-12-16 18:43:28 +0530

    Add files via upload

---
+++ Learning/index.js
@@ -0,0 +1,80 @@
+// let count=0
+// console.log(count)
+
+// let myAge=23
+// console.log(myAge)
+
+
+// let myAge=23
+// let humanDogRatio=2
+// humanDogRatio=myAge*humanDogRatio
+
+// let count=50
+// console.log(count)
+// count=count+50
+// console.log(count)
+// count=count-75
+// console.log(count)
+// count=count+45
+// console.log(count)
+
+
+// function increment(){
+//     console.log("This button was clicked")
+// }
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countdown()
+
+// countdown()
+
+// function adi(){
+//     console.log(42)
+// }
+
+// adi()
+
+//     let lap1=34
+//     let lap2=33
+//     let lap3=36
+
+// function sam() {
+//     let sam=lap1+lap2+lap3
+//     console.log(sam)
+// }
+// sam()
+
+
+
+// let lapsCompleted=0
+
+// function lap(){
+//     lapsCompleted=lapsCompleted+1
+// }
+// lap()
+// lap()
+// lap()
+ 
+// console.log(lapsCompleted)
+
+// countEl.innerText=count
+
+// let countEl=document.getElementById("count-el")
+
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+
+let count=0
+function increment(){
+    
+    count=count+1
+   countEl.innerText=count
+    console.log(count)
+}
+
